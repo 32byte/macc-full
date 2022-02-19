6fcd2d7c2ad48f40271c56fbf02a770fe9ad5bf3
32byte <xlebedenko@gmail.com>
2022-02-19 12:17:54 +0100

    Added more functions to the wesm

--- web/rust-wasm/src/lib.rs
+++ web/rust-wasm/src/lib.rs
@@ -1,6 +1,60 @@
 use wasm_bindgen::prelude::*;
+use macc_lib::{hex::ToHex, settings::Settings, blockchain::{utils, Transaction, TxStore, Block}};
+
+// utils
 
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello {}!", name)
+pub fn to_hex(data: &[u8]) -> String {
+    data.to_hex()
 }
+
+#[wasm_bindgen]
+pub fn calculate_mining_reward(block_height: usize) -> String {
+    utils::calculate_mining_reward(block_height, &Settings::default()).to_string()
+}
+
+#[wasm_bindgen]
+pub fn current_time() -> u64 {
+    macc_lib::utils::current_time()
+}
+
+// transaction
+#[wasm_bindgen]
+pub fn tx_hash(tx_str: String) -> Option<String> {
+    let tx: Transaction = serde_json::from_str(&tx_str).ok()?;
+
+    Some(tx.hash().ok()?.to_hex())
+}
+
+#[wasm_bindgen]
+pub fn tx_vout_total(tx_str: String) -> Option<String> {
+    let tx: Transaction = serde_json::from_str(&tx_str).ok()?;
+
+    Some(tx.vout_total().to_string())
+}
+
+#[wasm_bindgen]
+pub fn tx_vin_total(tx_str: String, store_str: String) -> Option<String> {
+    let tx: Transaction = serde_json::from_str(&tx_str).ok()?;
+    let store: TxStore = serde_json::from_str(&store_str).ok()?;
+
+    Some(tx.vin_total(&store)?.to_string())
+}
+
+#[wasm_bindgen]
+pub fn block_hash(block_str: String) -> Option<String> {
+    let block: Block = serde_json::from_str(&block_str).ok()?;
+
+    Some(block.hash(None).ok()?.to_hex())
+}
+
+// Wallet
+/*
+#[wasm_bindgen]
+pub fn my_utxos(store_str: String, sk_key: String) -> Option<String> {
+    let store: TxStore = serde_json::from_str(&store_str).ok()?;
+    let owned = store.get_owned(sk_key)?;
+
+    Some(serde_json::to_string(&owned).ok()?)
+}
+ */
